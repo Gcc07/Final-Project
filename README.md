0a40602e226ca6d391ebc2cded9cc6a5f1eb03d6
SirGuamsalot <gcardenasnc@icloud.com>
2025-04-30 14:01:47 -0400

    Added Task Adding

---
+++ README.md
@@ -0,0 +1,4 @@
+Challenges: 
+* I had trouble with getting the appending to work with the ending "done" part.
+Questions:
+* How do I get it all on the same line if I'm approaching it with a <p></p> seperation?
--- index.js
+++ index.js
@@ -11,11 +11,15 @@ const taskManager = document.getElementById("taskManager")
 let task_array = []
 let task_counter = 0
 
+
+
+
+
 class Task {
     id = 0
     name = ""
     priority = ""
-    isImportant = false
+    isImportant = importantSelector.value
     isCompleted = false
     date = Date()
     constructor(id, name, priority, isImportant,isCompleted,date) {
@@ -26,11 +30,20 @@ class Task {
         this.isCompleted = isCompleted
         this.date = date
     }
+    print() {
+        console.log(this.date)
+    }
   }
 
 
 
 addTaskButton.addEventListener("click", function() {
+
+    if (nameInput.value == "") {
+        alert("You must assign a task name first.")
+        return
+        
+    }
     for (let tasks in task_array) {
         if (tasks==null) {task_counter = 1}
         else {
@@ -38,8 +51,27 @@ addTaskButton.addEventListener("click", function() {
             task_counter = parseInt(tasks)+1
         }
     }
-    let pushed_task = new Task(task_counter, nameInput.value, prioritySelection.value, importantSelector.value, false, Date())
+    let pushed_task = new Task(task_counter, nameInput.value, prioritySelection.value, importantSelector.ariaChecked, false, Date())
         
     task_array.push(pushed_task)
     console.log(JSON.stringify(task_array));
+    pushed_task.print()
+
+    shown_task = document.createElement("task");
+    shown_task.innerHTML += `<p>${pushed_task.name}</p>` +  `<p>${pushed_task.priority}</p>` +  `<p>${pushed_task.date}</p>` + `<input type="checkbox" id="done"></input><label for="done">Done</label>` + `<button type="submit" id="deleteTaskButton">Delete</button>`;
+    console.log(pushed_task.isImportant == "on")
+    
+    if (pushed_task.isImportant == "on") {
+        shown_task.style.background = "red"
+    }
+    else {
+    }
+    
+    taskManager.appendChild(shown_task);
+
+    document.getElementById("deleteTaskButton").addEventListener("click", function() {
+        taskManager.removeChild(shown_task)
+    })
 })
+
+
